bcfd66dc19312caf06e02aebfe336e37b6d066d7
Sharthak Poudel <sharthakpoudel@Sharthaks-MacBook-Air.local>
2023-01-17 12:55:39 +0545

    boilerplate setup

---
+++ src/config/keys.ts
@@ -0,0 +1,6 @@
+export const keys = {
+  CORS_WHITELISTS: new RegExp(process.env.CORS_WHITELISTS),
+  LOGS: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  MONGO_URI: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TEST : process.env.MONGO_URI,
+  HONEY_BADGER_API: process.env.HONEY_BADGER_API
+};
